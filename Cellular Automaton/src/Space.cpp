dbfd1f98560c680704aa60d6d816759e51c7b5e6
SecT <tajnyt@gmail.com>
2014-03-24 14:29:02 +0100

    Added Cellular Automaton Eclipse project

---
+++ Cellular Automaton/src/Space.cpp
@@ -0,0 +1,20 @@
+/*
+ * Space.cpp
+ *
+ *  Created on: 19 mar 2014
+ *      Author: Grzesiek
+ */
+
+#include "Space.h"
+
+#include <stdio.h>
+
+Space::Space() {
+	// TODO Auto-generated constructor stub
+	matrix = NULL;
+}
+
+Space::~Space() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Cellular Automaton/src/Space.h
@@ -0,0 +1,20 @@
+/*
+ * Space.h
+ *
+ *  Created on: 19 mar 2014
+ *      Author: Grzesiek
+ */
+
+#ifndef SPACE_H_
+#define SPACE_H_
+
+class Space {
+private:
+	unsigned short int* matrix;
+
+public:
+	Space();
+	virtual ~Space();
+};
+
+#endif /* SPACE_H_ */
